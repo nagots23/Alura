5e8b9cb8e4527dd5b0599f8fa6af33c7396cbd49
GastonDiaz <91756323+nagots23@users.noreply.github.com>
2022-06-08 00:50:53 -0300

    Update encriptador.js

--- encriptador.js
+++ encriptador.js
@@ -1,46 +1,42 @@
-var texto = document.querySelector("#input-texto").value;
-
 //encriptar el texto
-function encriptar(texto){
+function encriptar(){
     var texto = document.querySelector("#input-texto").value;
     var textoCifrado = texto.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
-    document.querySelector(".entrada-texto").value = textoCifrado;
-    document.querySelector("#msg.box").value;
+    document.querySelector(".text-input-salida").value = textoCifrado;
+    document.querySelector("#input-texto").value;
 }
 
-var boton1 = document.querySelector("#encriptar-texto"); 
-    boton1.onclick = encriptar;
-    
-//Imprimir el resultado en el input
-console.log(textoEncriptado);
-var textoSalida = document.querySelector("#msg");
-textoSalida.value = textoEncriptado;
-
-
-var botonEncriptar = document.querySelector("#btn-encriptar");
-botonEncriptar.addEventListener("click",function(event){
-    event.preventDefault();
-    var textoEntrada = document.querySelector("#input-texto").value;
-    encriptarTexto(textoEntrada);
-});
+var boton1 = document.querySelector("#boton-encriptar-texto"); boton1.onclick = encriptar;
 
 
 //desencriptar el texto
-function desencriptar(texto){ 
-    var texto = document.querySelector("#input-texto").value; 
+function desencriptar(){ 
+    var texto = document.querySelector("#input-texto").value;
     var textoDesencriptado = texto.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ai/gi, "a").replace(/ober/gi, "o").replace(/ufat/gi, "u"); 
-    document.querySelector(".entrada-texto").value = textoDesencriptado; 
-    document.querySelector("#msg-box").value;
+    document.querySelector(".text-input-salida").value = textoDesencriptado;
+    document.querySelector("#input-texto").value;
 }
 
-//Imprimir el resultado en el input
-     console.log(textoDesencriptado);
-     var textoSalida = document.querySelector("#msg.box");
-     textoSalida.value = textoDesencriptado;
 
+var boton2 = document.querySelector("#boton-desencriptar-texto"); boton2.onclick = desencriptar;
 
-var desencriptar = document.querySelector("#desencriptar-texto");
-    boton2.onclick = desencriptar;
+function copiar(){
+    var textCopy = document.getElementById("resultado");
+    textCopy.select();
+    document.execCommand("copy");
+    alert("Texto copiado!");
+}
+
+var botonCopiar = document.querySelector("#copiar-texto");
+botonCopy.addEventListener("click",function(event){
+    event.preventDefault();
+    var cajaTexto = document.querySelector("#resultado");
+    var texto = cajaTexto.value;
+    checkTextoUsuario(texto);
+    copiar();
+    limpiarCampo(cajaTexto);      
+    return;
+});
 
 
 
